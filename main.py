4c2d16b0598331332611d613e5560644da61a6db
gregshestakov <gregshestakov@gmail.com>
2022-10-31 00:15:02 +0200

    Improve speed, add several NOs choosing feature

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 import streamlit as st
 import scripts
-import datetime
 import plotly.express as px
 import pandas as pd
 
 st.title('Validators performance exctractor')
+
+if 'val_NO_choice' not in st.session_state:
+    val_NO_list= []
+    st.session_state['val_NO_choice'] = val_NO_list
+
+if 'val_choice' not in st.session_state:
+    val_list= []
+    st.session_state['val_choice'] = val_list
+
+if 'one_NO_choice' not in st.session_state:
+    val_list= []
+    st.session_state['one_NO_choice'] = val_list
 
 operator_list=['Allnodes',
  'Anyblock Analytics',
@@ -36,26 +47,34 @@ operator_list=['Allnodes',
  'Staking Facilities',
  'stakefish']
 
-operator_select=st.selectbox(
+
+st.write('**One NO info (with visualization)**')
+one_NO_select=st.selectbox(
         "Please select operator",
-    operator_list)
+    operator_list,key='one_NO_select')
 
-start_date=st.date_input("Choose start date")
+start_date=st.date_input("Choose start date",key='one_NO_start_date')
 
-end_date = st.date_input("Choose end date")
+end_date = st.date_input("Choose end date",key='one_NO_end_date')
 
 st.write('Please remember that each request cost approximately 6 dollars and processing time is about 2.5 minutes per 1 day per 1 NO')
 if st.button('Get info'):
 
- start_epoch=scripts.convert_date_to_epoch(str(start_date))+1
- end_epoch = scripts.convert_date_to_epoch(str(end_date))+225
+
+ one_no_choice_dict={}
+ one_no_choice_dict['start_date']=str(start_date)
+ one_no_choice_dict['end_date']=str(end_date)
+ one_no_choice_dict['node_operator'] = one_NO_select
+ st.session_state['one_NO_choice'].append(one_no_choice_dict)
+
 
  st.write('Start fetching data')
- rewards_df=scripts.create_rewards_table(start_epoch=start_epoch,end_epoch=end_epoch,operator_name=operator_select)
+
+ rewards_df=scripts.create_rewards_table(st.session_state['one_NO_choice'])
  st.write('All data are fetched, prepare visualization')
 
  processed_reward_df=scripts.process_rewards_table(rewards_df)
- st.write(f'Results for {operator_select} from {start_date} to {end_date}')
+ st.write(f'Results for {one_NO_select} from {start_date} to {end_date}')
  total_result_chart = px.bar(processed_reward_df, x='epoch_date', y=['total_rewards', 'total_penalties', 'total_missed_rewards'],
                              title='Totals')
  rewards_chart = px.bar(processed_reward_df, x='epoch_date',
@@ -80,18 +99,54 @@ if st.button('Get info'):
  st.write('WARNING - once you download data, all graphs will disappear')
  st.download_button(label='Download info',
                     data=csv_data_validators,
-                    file_name=f'{operator_select} from {start_date} to {end_date}.csv',
-                    mime='text/csv')
+                    file_name=f'{one_NO_select} from {start_date} to {end_date}.csv',
+                    mime='text/csv',
+                    key='one_NO_download')
+
+st.markdown("""---""")
+st.write('**Several NO info (just data)**')
+
+st.write('Choose NO and date range and press "Add" button, once you choose all NO and date range for them press "Get info" button')
+
+
+several_NO_select = st.selectbox(
+        "Please select operator",
+        operator_list, key='multiple_NO_select')
+several_NO_start_date = st.date_input("Choose start date",key='multiple_NO_start_date')
+
+several_NO_end_date = st.date_input("Choose end date",key='multiple_NO_end_date')
+add_NO=st.button('Add',key='multiple_NO_add')
+
+
+if add_NO:
+    choice={}
+    choice['node_operator']=several_NO_select
+    choice['start_date']= str(several_NO_start_date )
+    choice['end_date']=str(several_NO_end_date)
+    st.session_state['val_NO_choice'].append(choice)
+
+st.write('You already choose:')
+st.write(st.session_state['val_NO_choice'])
+
+get_multiple_NO_info=st.button('Get info',key='multiple_NO_download')
+if get_multiple_NO_info:
+    st.write('Start fetching data')
+    NOs_rewards_df=scripts.create_rewards_table(st.session_state['val_NO_choice'])
+    processed_NOs_rewards_df=scripts.process_rewards_table(NOs_rewards_df)
+    st.write(processed_NOs_rewards_df.head(10))
+    csv_NOs_data_validators = processed_NOs_rewards_df.to_csv(index=False).encode('utf-8')
+    st.download_button(label='Download info',
+                           data=csv_NOs_data_validators,
+                           file_name=f'Multiple Nos data .csv',
+                           mime='text/csv',
+                           key='multiple_NO_download' )
 
 
 
+
+
+st.markdown("""---""")
+st.write('This is test section')
 if st.button('Test'):
     validators=scripts.get_validators_by_epoch()
     st.table(validators.head(10))
-
-
-
-
-
-
-
--- scripts.py
+++ scripts.py
@@ -14,18 +14,20 @@ CREDENTIALS = service_account.Credentials.from_service_account_info(TOKEN)
 CLIENT = bigquery.Client(credentials=CREDENTIALS, project=CREDENTIALS.project_id)
 
 REWARDS_QUERY="""
-with coefs as (
-  SELECT 
+with coefs as(
+SELECT  
 f_epoch,
+f_attesting_balance/f_active_balance as source_coef,
 f_target_correct_balance/f_active_balance as target_coef,
-f_head_correct_balance/f_active_balance as head_coef,
-f_attesting_balance/f_active_balance as source_coef
+f_head_correct_balance/f_active_balance as head_coef
 
- FROM `high-hue-328212.chaind.t_epoch_summaries` 
+FROM `high-hue-328212.chaind.t_epoch_summaries` 
+
 )
 
 SELECT 
-DATE_TRUNC(TIMESTAMP_SECONDS(1606824023 + (rewards.f_epoch * 12 * 32)),DAY) as epoch_date,
+DATE_TRUNC(TIMESTAMP_SECONDS(1606824023 + (epoch_data.f_epoch * 12 * 32)),DAY) as epoch_date,
+node_operators.f_operator_name,
 sum(reward_for_proposal) as total_rewards_for_proposal,
 sum(reward_for_source*source_coef) as total_rewards_for_source,
 sum(reward_for_target*target_coef) as total_rewards_for_target,
@@ -40,12 +42,10 @@ count(nullif(reward_for_proposal,0)) as count_proposals,
 count(nullif(missed_reward_for_proposal,0)) as count_missed_proposals,
 count(nullif(penalty_for_source,0)) as count_missed_for_source,
 count(nullif(penalty_for_target,0)) as count_missed_for_target
-FROM `high-hue-328212.chaind.mv_validators_rewards_per_epoch` as rewards inner join coefs on coefs.f_epoch=rewards.f_epoch
+FROM `high-hue-328212.chaind.mv_validators_rewards_per_epoch` as epoch_data
+inner join coefs on epoch_data.f_epoch = coefs.f_epoch
+inner join  `high-hue-328212.chaind.t_operators` as node_operators on epoch_data.f_validator_index=node_operators.f_index
 where 
-f_epoch between @start_epoch and @end_epoch
-and
-f_validator_index in (SELECT f_index FROM `high-hue-328212.chaind.t_operators` where f_operator_name=@operator_name)
-group by  epoch_date
 """
 
 
@@ -80,19 +80,9 @@ def convert_date_to_epoch(date):
     epoch= (epoch_ts-1606824023+10800)/(12*32)
     return int(epoch)
 
-def create_rewards_table(start_epoch,end_epoch,operator_name):
-
-    job_config = bigquery.QueryJobConfig(
-        query_parameters=[
-            bigquery.ScalarQueryParameter("start_epoch", "INT64", start_epoch),
-            bigquery.ScalarQueryParameter("end_epoch", "INT64", end_epoch),
-            bigquery.ScalarQueryParameter("operator_name", "STRING", operator_name)
-        ]
-    )
-
-    query_job = CLIENT.query(REWARDS_QUERY, job_config=job_config)
-
-
+def create_rewards_table(choice_dict):
+    FINAL_QUERY=create_query(choice_dict)
+    query_job = CLIENT.query(FINAL_QUERY)
     rewards_df=query_job.to_dataframe()
     return rewards_df
 
@@ -131,3 +121,24 @@ def get_validators_by_epoch ():
     result = validators_df[['f_epoch', 'active_validators', 'total_validators']]
     result = result[result['f_epoch'] >= 146875]
     return result
+
+def create_where_clause(choise_dict):
+    where_clause=""
+    for x in range(len(choise_dict)):
+        start_epoch=convert_date_to_epoch(choise_dict[x]['start_date'])+1
+        end_epoch=convert_date_to_epoch(choise_dict[x]['end_date'])+225
+        operator=choise_dict[x]['node_operator']
+        query=f"(epoch_data.f_epoch between {start_epoch} and {end_epoch} and f_validator_index in (SELECT f_index FROM `high-hue-328212.chaind.t_operators` where f_operator_name='{operator}'))"
+        where_clause+=query
+        if x!= len(choise_dict)-1:
+            where_clause+=' or '
+        if x  == len(choise_dict)-1:
+            where_clause+=' group by 1,2'
+
+    return where_clause
+
+
+def create_query(choice_dict):
+    where_clauses=create_where_clause(choice_dict)
+    final_query=REWARDS_QUERY+where_clauses
+    return final_query
